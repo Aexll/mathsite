de8c46e925fcace76ae0394c2ad197b6d9b8adf1
axel <axellavignepro@gmail.com>
2025-06-23 09:09:11 +0200

    font ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Here so latex : $x^2 + y^2 = z^2$</p>
+<h1 style="color: #1f2937">
+	Exercice 3 : Théorème du point fixe de Knaster-Tarski (cas particulier)
+</h1>
+<p style="color: red">
+	Soit $f$ une fonction de $X$ dans $X$ et $f$ admet un point fixe si il existe $x \in X$ tel que $f(x) = x$.
+</p>
+<p>Soit $f$ une fonction de $X$ dans $X$ et $f$ admet un point fixe si il existe $x \in X$ tel que $f(x) = x$.</p>
